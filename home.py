108d58b6e724d19ba67a5ffe09d435a258995973
vnandini-4edu <vnandini.4education@gmail.com>
2025-02-07 19:10:56 +0530

    Successfully printing current time

---
+++ home.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+print("This is first line of home.py")
+print(datetime.now())
+
+# Vishnu should complete this code
+# Requirment: Read all the content from data/source1.txt
+
+
